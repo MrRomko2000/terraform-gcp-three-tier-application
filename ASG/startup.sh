37ae27927113bcca78d8699de64466d98c5aca92
Diego <diegosolutions33@gmail.com>
2023-06-16 01:52:43 +0000

    update

--- ASG/startup.sh
+++ ASG/startup.sh
@@ -1,8 +1,10 @@
 # #!/bin/bash
+#This line specifies the IP address of the host where the WordPress database is located. In this case, the database is hosted on the IP address "34.148.150.3".
 WORDPRESS_DB_HOST="34.23.161.22"
 WORDPRESS_DB_NAME="team3db"
 WORDPRESS_DB_USER="team3user"
 WORDPRESS_DB_PASSWORD="gcpteam3project2023##"
+#This line uses the yum package manager to install several packages: httpd (Apache web server), wget (command-line tool for downloading files), unzip (tool for extracting compressed files), epel-release (Extra Packages for Enterprise Linux repository), and mysql (MySQL database server).
 sudo yum install httpd wget unzip epel-release mysql -y
 # This line installs the Remi repository, which provides updated PHP packages for CentOS
 sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
