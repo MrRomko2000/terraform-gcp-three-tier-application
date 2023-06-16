48f7840b5b7dada54b61b292c308e6372b6dfa6d
Diego <diegosolutions33@gmail.com>
2023-06-16 01:49:23 +0000

    update

--- ASG/startup.sh
+++ ASG/startup.sh
@@ -1,9 +1,10 @@
 # #!/bin/bash
-WORDPRESS_DB_HOST="34.148.150.3"
+WORDPRESS_DB_HOST="34.23.161.22"
 WORDPRESS_DB_NAME="team3db"
 WORDPRESS_DB_USER="team3user"
 WORDPRESS_DB_PASSWORD="gcpteam3project2023##"
 sudo yum install httpd wget unzip epel-release mysql -y
+# This line installs the Remi repository, which provides updated PHP packages for CentOS
 sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 sudo yum -y install yum-utils
 sudo yum-config-manager --enable remi-php56   [Install PHP 5.6]
